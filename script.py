14a465d30d5f39eaa5fe6398dfc1fc8c91c4a69d
dannjose <Danilo.Santos@wolterskluwer.com>
2023-02-06 17:04:24 +0000

    Adding remove function

--- script.py
+++ script.py
@@ -1,14 +1,23 @@
 def my_function():
-  # Full title should be no longer than len (e.g.25) symbols
   var = "Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only"
-  title = str("25")
+  title = str(25)
+
+  # Full title should be no longer than len (e.g.25) symbols
   print(var[0:25])
 
   #If title longer than len (e.g.25) symbols than you need to show “...” at the end
   if (var) > title:
     print(var,"...")
 
+  elif(var) < title:
+    print("Less than 25 characters")
+
   #Title should end on full word
+  def remove(string):
+    return string.replace(" ", "")
+
+  # Driver Program
+  print(remove(var))
   
 
 my_function()
